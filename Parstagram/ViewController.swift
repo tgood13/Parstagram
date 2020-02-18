c6cbcd2e64b4532805a2ba099e18955c2e0e0f82
Troy Good <troygood13@gmail.com>
2020-02-17 17:26:54 -0800

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Troy Good on 2/17/20.
+//  Copyright © 2020 tgood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
